2489238a4e98649ca3c0dfeafaeeda2887c80cb3
antonshe84 <antonshe84@gmail.com>
2021-03-28 11:20:35 +0300

    final

--- task_7.py
+++ task_7.py
@@ -5,3 +5,37 @@
 Проверьте корректность полученного результата.
 """
 
+
+class Complex:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __str__(self):
+        sign = '+' if self.b > 0 else '-'
+        return f"{self.a} {sign} {abs(self.b)}i"
+
+    def __add__(self, other):
+        return Complex(self.a + other.a, self.b + other.b)
+
+    def __sub__(self, other):
+        return Complex(self.a - other.a, self.b - other.b)
+
+    # (a + bi)*(c + di) = ac + bdi + adi + bdi^2
+    # где i^2 =  -1
+    def __mul__(self, other):
+        a = self.a
+        b = self.b
+        c = other.a
+        d = other.b
+        return Complex(a * c + (-1) * b * d, b * c + a * d)
+
+
+c1 = Complex(4, 3)
+c2 = Complex(5, 6)
+print(f"c1: {c1}, c2: {c2}")
+print(f"Сумма: {c1 + c2}")
+print(f"Разность: {c1 - c2}")
+c3 = c1 * c2
+print(f"Произведение: {c3}")
+print(type(c3))
